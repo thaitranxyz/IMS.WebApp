449bfa4bfd42f90ceb69ed952a11c4933d293d07
thaitran <43898236+thaitranxyz@users.noreply.github.com>
2023-10-08 23:01:05 +1100

    Add project files.

---
+++ IMS.Plugins.EFCore/InventoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.PluginInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace IMS.Plugins.EFCore
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private readonly IMSContext db;
+
+        public InventoryRepository(IMSContext db)
+        {
+            this.db = db;
+        }
+        public async Task<IEnumerable<Inventory>> GetInventoriesByName(string name)
+        {
+            return await this.db.Inventories.Where(x => x.InventoryName.Contains(name) || string.IsNullOrWhiteSpace(name)).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventories()
+        {
+            return await this.db.Inventories.ToListAsync();
+        }
+    }
+}
---
+++ IMS.UseCases/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,27 @@
+﻿using IMS.CoreBusiness;
+using IMS.UseCases.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+using System.Xml.Linq;
+
+namespace IMS.UseCases
+{
+    public class ViewInventoriesByNameUseCase : IViewInventoriesByNameUseCase
+    {
+        private readonly IInventoryRepository inventoryRepository;
+
+        public ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository)
+        {
+            this.inventoryRepository = inventoryRepository;
+        }
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+        {
+            return await this.inventoryRepository.GetInventoriesByName(name);
+        }
+
+        public async Task<IEnumerable<Inventory>> GetInventories()
+        {
+            return await this.inventoryRepository.GetInventories();
+
+        }
+    }
+}
